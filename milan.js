df3843fd75797df346e06dd8c6d4afeb5367cbaf
SEELIHAT <etumanov77@gmail.com>
2024-04-28 02:56:54 -0700

    js new

--- milan.js
+++ milan.js
@@ -1,5 +1,5 @@
 
-  function sendMessageToWhatsApp(phoneNumber) {
+  /*function sendMessageToWhatsApp(phoneNumber) {
     var message = "Hello, I would like to order.";
 
     // Replace spaces with %20 for URL
@@ -8,7 +8,24 @@
     var url = "https://wa.me/" + phoneNumber + "?text=" + message;
 
     window.open(url, '_blank');
+  }*/
+  function sendMessageToWhatsApp(phoneNumber) {
+    var message = "Hello, I would like to order.";
+  
+    // Get the text content from the <p class="order"> element
+    var additionalMessage = document.querySelector('.order').textContent;
+  
+    // Append the additional message to the main message
+    message += " " + additionalMessage;
+  
+    // Replace spaces with %20 for URL
+    message = encodeURIComponent(message);
+  
+    var url = "https://wa.me/" + phoneNumber + "?text=" + message;
+  
+    window.open(url, '_blank');
   }
+  
 
 
 
